b6660265ea08ebcf24e99a13805d05017b5ff696
YahefuYusufu <parguzatt@gmail.com>
2024-11-21 22:25:32 +0100

    splash has added

--- app/src/main/java/com/prg/imagefactory/MainActivity.kt
+++ app/src/main/java/com/prg/imagefactory/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.prg.imagefactory.ui.theme.ImageFactoryTheme
 import com.prg.imagefactory.presentation.navigation.NavGraphSetup
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             ImageFactoryTheme {
